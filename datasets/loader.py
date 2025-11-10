d8c0c46b80452fcf9232770fa5f6b79fc0a34c36
vaganapathy <142136594+vaganapathy@users.noreply.github.com>
2025-11-11 05:29:09 +0530

    PIN_MEMORY

--- datasets/loader.py
+++ datasets/loader.py
@@ -27,7 +27,8 @@ def construct_loader(cfg, split):
         shuffle=shuffle,
         num_workers=cfg.DATA_LOADER.NUM_WORKERS,
         pin_memory=cfg.DATA_LOADER.PIN_MEMORY,
-        drop_last=drop_last
+        drop_last=drop_last,
+        persistent_workers=True,
     )
 
     return loader
